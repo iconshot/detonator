2d6c5420e242dbc3b890c3a939fc9c0a010d47fd
josecarlosrx <jc@iconshot.com>
2024-11-05 16:37:42 -0500

    adjust size on keyboard opened

--- ios/Sources/Detonator/Layout/ViewLayoutController.swift
+++ ios/Sources/Detonator/Layout/ViewLayoutController.swift
@@ -4,4 +4,36 @@ class ViewLayoutController: UIViewController {
     override func loadView() {
         view = ViewLayout()
     }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+
+    @objc func keyboardWillShow(notification: NSNotification) {
+        let view = view as! ViewLayout
+        
+        if let keyboardFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect {
+            view.frame.size.height -= keyboardFrame.height
+            
+            view.performLayout()
+        }
+    }
+
+    @objc func keyboardWillHide(notification: NSNotification) {
+        let view = view as! ViewLayout
+
+        if let keyboardFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect {
+            view.frame.size.height += keyboardFrame.height
+            
+            view.performLayout()
+        }
+    }
 }
